2d1ef33731386b57f3ae7f7277fae3f2addc4fee
divya-gitrep <divya.shky@gmail.com>
2025-11-11 18:12:26 +0530

    testing

---
+++ src/test/java/com/volvo/base/BaseTest.java
@@ -0,0 +1,36 @@
+package com.volvo.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected Dotenv dotenv;
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        // load .env
+        dotenv = Dotenv.load();
+
+        // setup driver (Chrome example)
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
