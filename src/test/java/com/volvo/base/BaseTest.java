901a2d79e615e863e6227e2ddc5321f894b4a9b6
divya-gitrep <divya.shky@gmail.com>
2025-11-12 10:53:24 +0530

    Added new Salesforce login test and updated test configuration

--- src/test/java/com/volvo/base/BaseTest.java
+++ src/test/java/com/volvo/base/BaseTest.java
@@ -1,5 +1,38 @@
 package com.volvo.base;
-public class BaseTest{
 
-    /*test */
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected Dotenv dotenv;
+
+    @BeforeClass(alwaysRun = true)
+    public void beforeClass() {
+        // load .env
+        dotenv = Dotenv.load();
+
+        // setup driver (Chrome example)
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
